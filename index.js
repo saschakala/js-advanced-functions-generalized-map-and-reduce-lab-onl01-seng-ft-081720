69bf8935f54cc86b6e25be662d0795e1cc9e0473
“saschakala” <mxsaschakala@gmail.com>
2020-12-02 23:14:02 -0500

    commit tree 7a3a12c3cbb5aa6bcaffdfc2c43fb6600dc76ede

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your functions here
+function map(arr, callback) {
+    return arr.map(i => callback(i));
+};
+
+function reduce(arr, callback, start) {
+    let s = !!start ? start : arr[0]
+    let i = (!!start) ? 0 : 1
+
+    for (; i < arr.length; i++) {
+        s= callback(arr[i], s)
+    }
+
+    return s;
+}
