c6f4b991011354ea73ab22bdfd945ca2cac1d623
“saschakala” <mxsaschakala@gmail.com>
2020-12-02 23:32:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Add your functions here
+function map(arr, callback) {
+    return arr.map(i => callback(i));
+};
+
+const reduce = (sourceArray, func, startingPoint) => {
+    let r = (!!startingPoint) ? startingPoint : sourceArray[0]
+    let i = (!!startingPoint) ? 0 : 1
+    for (; i < sourceArray.length; i++) {
+      r = func(sourceArray[i], r)
+    }
+    return r;
+  }
